1bd9313053df249d9b86b7868436f6621c23df14
denashams <dena.shamsizadeh@gmail.com>
2023-03-07 18:21:48 +0330

    Added 3dslider component

---
+++ src/components/slider3d/Slider3d.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+import { Pagination, EffectCoverflow } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "./Slider3d.css";
+
+import pic1 from "../../assets/flashcards/1.jpg";
+import pic2 from "../../assets/flashcards/2.jpg";
+import pic3 from "../../assets/flashcards/3.jpg";
+import pic4 from "../../assets/flashcards/4.jpg";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
+
+export default function Slider3d() {
+  return (
+    <div className="photo-container">
+      <Swiper
+        modules={[Pagination, EffectCoverflow]}
+        spaceBetween={20}
+        slidesPerView="auto"
+        pagination={{ clickable: true }}
+        effect="coverflow"
+        grabCursor="true"
+        centeredSlides="true"
+        loop="true"
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 100,
+          depth: 300,
+        }}
+        breakpoints={{
+          700: {
+            spaceBetween: 0,
+            slidesPerView: 4,
+          },
+          500: {
+            spaceBetween: 100,
+            slidesPerView: 2,
+          },
+          411: {
+            spaceBetween: 100,
+            slidesPerView: 2,
+          },
+          300: {
+            spaceBetween: 0,
+            slidesPerView: 1,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <img src={pic1} alt="pic1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={pic2} alt="pic2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={pic3} alt="pic3" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={pic4} alt="pic4" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Slider from "../../components/header-slider/Slider";
 import facade from "../../assets/facade.jpg";
-import StackedCards from "../../components/stacked-cards/StackedCards";
 import { MdKeyboardVoice } from "react-icons/md";
+import Slider3d from "../../components/slider3d/Slider3d";
 
 import "./Home.css";
 
@@ -34,26 +34,26 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <h2 className="mt-5">فلش کارت</h2>
-      <section>
-        <div className="container item-container mt-5 p-5">
-          <div className="row">
-            <div className="col-md item">
-              <p>
-                مان‌ها و دیگر سازه‌های کالبدی است. معماری و دیگر رشته‌های علمی
-                طراحی در پیوند با آن، مانند طراحی شهری، معماری منظر، طراحی داخلی
-                و … در کنار هم، طراحی محیط را به‌وجود می‌آورند. گستردگی و
-                گوناگونی بوجودآمده در آثار معماری معاصر و ناسازگاری که گاهی،
-                میان آنها دیده می‌شود؛
-              </p>
-              <button className="button-bold mt-4">ورود</button>
-            </div>
-            <div className="col-md ">
-              <StackedCards />
-            </div>
+      <div >
+        <h2 className="mt-5">فلش کارت</h2>
+        <section>
+          <div className=" mt-4 container">
+            <p className="flashcard-description mb-5">
+              مان‌ها و دیگر سازه‌های کالبدی است. معماری و دیگر رشته‌های علمی
+              طراحی در پیوند با آن، مانند طراحی شهری، معماری منظر، طراحی داخلی و
+              … در کنار هم، طراحی محیط را به‌وجود می‌آورند. گستردگی و گوناگونی
+              بوجودآمده در آثار معماری معاصر و ناسازگاری که گاهی، میان آنها دیده
+              گستردگی و گوناگونی بوجودآمده در آثار معماری معاصر و ناسازگاری که
+              گاهی، میان آنها دیده می‌شود؛
+            </p>
           </div>
-        </div>
-      </section>
+
+          <div className="mt-5 mb-5">
+            <Slider3d />
+          </div>
+            <button className="button-bold mt-4">ورود</button>
+        </section>
+      </div>
       <h2 className="mt-5">پادکست</h2>
       <section>
         <div className="container item-container mt-5 p-5">
