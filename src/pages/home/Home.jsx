b773fef04897511f25f3446345c90ba7b9367977
denashams <dena.shamsizadeh@gmail.com>
2023-03-08 15:59:57 +0330

    fixed spacing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,46 +4,48 @@ import facade from "../../assets/facade.jpg";
 import Slider3d from "../../components/slider3d/Slider3d";
 import MobileNav from "../../components/mobile-navbar/MobileNav";
 
-
 import "./Home.css";
 import AudioCard from "../../components/audio-card/AudioCard";
 
 export default function Home() {
   return (
     <div className="home">
+      <div>
+        <MobileNav />
+      </div>
       <div className="header-slider">
         <Slider />
       </div>
-        <div>
-      <MobileNav/>
-      </div>
 
       <section id="contentcard">
-      <h2 className="mt-5">محتوای متنی</h2>
-        <div className="container item-container p-0 mt-5">
-          <div className="row">
-            <div className="col-md item">
-              <img src={facade} alt="architecture" />
-            </div>
-            <div className="col-md item">
-              <p>
-                معماری (مهرازی، والادگری یا رازیگری)، شیوه طراحی و روش ساخت و
-                ساز ساختمان‌ها و دیگر سازه‌های کالبدی است. معماری و دیگر
-                رشته‌های علمی طراحی در پیوند با آن، مانند طراحی شهری، معماری
-                منظر، طراحی داخلی و … در کنار هم، طراحی محیط را به‌وجود
-                می‌آورند. گستردگی و گوناگونی بوجودآمده در آثار معماری معاصر و
-                ناسازگاری که گاهی، میان آنها دیده می‌شود؛ نشان از گم‌شدگی تعریف
-                روشن معماری، در صد سال گذشته دارد
-              </p>
-              <button className="button-bold mt-4">ورود</button>
+        <div className="container mt-5">
+          <h2>محتوای متنی</h2>
+
+          <div className="item-container mt-5 p-0">
+            <div className="row">
+              <div className="col-md item">
+                <img src={facade} alt="architecture" />
+              </div>
+              <div className="col-md item">
+                <p>
+                  معماری (مهرازی، والادگری یا رازیگری)، شیوه طراحی و روش ساخت و
+                  ساز ساختمان‌ها و دیگر سازه‌های کالبدی است. معماری و دیگر
+                  رشته‌های علمی طراحی در پیوند با آن، مانند طراحی شهری، معماری
+                  منظر، طراحی داخلی و … در کنار هم، طراحی محیط را به‌وجود
+                  می‌آورند. گستردگی و گوناگونی بوجودآمده در آثار معماری معاصر و
+                  ناسازگاری که گاهی، میان آنها دیده می‌شود؛ نشان از گم‌شدگی
+                  تعریف روشن معماری، در صد سال گذشته دارد
+                </p>
+                <button className="button-bold mt-4">ورود</button>
+              </div>
             </div>
           </div>
         </div>
       </section>
       <div>
         <section id="imagecard">
-        <h2 className="mt-5">فلش کارت</h2>
-          <div className=" mt-4 container">
+          <div className=" mt-5 container">
+            <h2 className="mb-4">فلش کارت</h2>
             <p className="flashcard-description mb-5">
               مان‌ها و دیگر سازه‌های کالبدی است. معماری و دیگر رشته‌های علمی
               طراحی در پیوند با آن، مانند طراحی شهری، معماری منظر، طراحی داخلی و
@@ -61,8 +63,8 @@ export default function Home() {
         </section>
       </div>
       <section id="podcast">
-      <h2 className="mt-5">پادکست</h2>
-        <div className="container mt-4">
+        <div className="container">
+        <h2 className="mt-5 mb-4">پادکست</h2>
           <p className="podcast-description">
             مان‌ها و دیگر سازه‌های کالبدی است. معماری و دیگر رشته‌های علمی طراحی
             در پیوند با آن، مانند طراحی شهری، معماری منظر، طراحی داخلی و … در
@@ -70,7 +72,9 @@ export default function Home() {
             بوجودآمده در آثار معماری معاصر و ناسازگاری که گاهی، میان آنها دیده
             می‌شود؛
           </p>
+          <div className="mt-5">
           <AudioCard />
+          </div>
           <button className="button-bold mt-4">ورود</button>
         </div>
       </section>
