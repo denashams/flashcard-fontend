ca5566d4d7453be4d689b01c875d50ca998968e6
denashams <dena.shamsizadeh@gmail.com>
2023-03-13 20:59:59 +0330

    edited login page css

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -7,47 +7,50 @@ export default function Login() {
 
   return (
     <div className="Login">
-    <div className={`logincontainer ${addClass}`} id="loginContainer">
-      <div className="form-container signup-container">
-        <form>
-          <h2>ساختن حساب کاربری</h2>
-          <input type="text" placeholder="نام" />
-          <input type="email" placeholder="ایمیل" />
-          <input type="password" placeholder="گذرواژه" />
-          <input type="button" value="ثبت نام" className="signup-btn" />
-        </form>
-      </div>
-      <div className="form-container signin-container">
-        <form>
-          <h2>ورود به حساب کاربری</h2>
-          <input type="text" id="username" placeholder="نام کاربری" />
-          <input type="password" placeholder="گذرواژه" />
-          <input type="button" value="ورود" className="signin-btn"/>
-        </form>
-      </div>
-      <div className="overlay-container">
-        <div className="overlay">
-          <div className="overlay-panel overlay-left">
-            <button
-              className="ghost"
-              id="signIn"
-              onClick={() => setAddClass("")}
-            >
-              وارد شوید
-            </button>
-          </div>
-          <div className="overlay-panel overlay-right">
-            <button
-              className="ghost"
-              id="signUp"
-              onClick={() => setAddClass("right-panel-active")}
-            >
-              عضو شوید
-            </button>
+      <div className={`logincontainer ${addClass}`} id="loginContainer">
+        <div className="form-container signup-container">
+          <form>
+            <h2>ساختن حساب کاربری</h2>
+            <input type="text" placeholder="نام کاربری" />
+            <input type="email" placeholder="ایمیل" />
+            <input type="password" placeholder="گذرواژه" />
+            <input type="button" value="ثبت نام" className="signup-btn" />
+          </form>
+        </div>
+        <div className="form-container signin-container">
+          <form>
+            <h2>ورود به حساب کاربری</h2>
+            <input type="text" id="username" placeholder="نام کاربری" />
+            <input type="password" placeholder="گذرواژه" />
+            <input type="button" value="ورود" className="signin-btn" />
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h3>حساب کاربری ندارید؟</h3>
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={() => setAddClass("")}
+              >
+                عضو شوید
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h3>قبلاً عضو شده‌اید؟</h3>
+
+              <button
+                className="ghost"
+                id="signUp"
+                onClick={() => setAddClass("right-panel-active")}
+              >
+                وارد شوید
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
