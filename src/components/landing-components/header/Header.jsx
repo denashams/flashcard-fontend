4760203296411f5dad08ec06cd59b54d0d3f8bda
denashams <dena.shamsizadeh@gmail.com>
2023-06-11 21:43:37 +0200

    Added coming soon links

--- src/components/landing-components/header/Header.jsx
+++ src/components/landing-components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./header.css";
 import logoWhite from "../../../assets/logo-white.png";
 // import HeaderSocials from "./HeaderSocials";
@@ -10,7 +11,7 @@ const Header = () => {
         <h1>مان تحقیق و نظر آویس</h1>
         <h5>همراه شما در پژوهش</h5>
         
-        <button class="brochure-btn"><span>دانلود بروشور</span></button>
+        <button class="brochure-btn"><span><NavLink to="coming-soon">دانلود بروشور</NavLink></span></button>
         {/* <HeaderSocials /> */}
         <div className="main-image">
           <img src={logoWhite} alt="matn avis logo" />
