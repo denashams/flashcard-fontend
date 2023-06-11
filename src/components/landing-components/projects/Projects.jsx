0d1526c3cea67cbe27391255387e4d5bd383d24a
denashams <dena.shamsizadeh@gmail.com>
2023-06-11 22:33:26 +0200

    fixed project section on landing page

--- src/components/landing-components/projects/Projects.jsx
+++ src/components/landing-components/projects/Projects.jsx
@@ -9,18 +9,12 @@ import projects from "../../../assets/projects.jpg";
 // import pirnia from "../../../assets/pirnia.jpg";
 // import page from "../../../assets/page.jpg";
 // import note from "../../../assets/note.jpg";
-import { BiNetworkChart } from "react-icons/bi"
-import { TbBinaryTree } from "react-icons/tb"
-import { IoIosGitNetwork } from "react-icons/io"
-import { FaChalkboardTeacher } from "react-icons/fa"
-import { BsReverseListColumnsReverse } from "react-icons/bs"
-import { SlNotebook } from "react-icons/sl"
-
-
-
-
-
-
+import { BiNetworkChart } from "react-icons/bi";
+import { TbBinaryTree } from "react-icons/tb";
+import { IoIosGitNetwork } from "react-icons/io";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { BsReverseListColumnsReverse } from "react-icons/bs";
+import { SlNotebook } from "react-icons/sl";
 
 const Projects = () => {
   return (
@@ -29,12 +23,15 @@ const Projects = () => {
         <div className="row">
           <div className="col-4 project-image ">
             <img src={projects} alt="" />
+            <div className="project-title"><h2>پروژه‌های ما</h2></div>
           </div>
           <div className="col-8 pt-5 ps-5">
             <div className="row row-container">
               <div className="col-4 ">
                 <div className="single-project  ">
-                  <span className="project-icon"><BiNetworkChart/></span>
+                  <span className="project-icon">
+                    <BiNetworkChart />
+                  </span>
                   <h6>هستان‌شناسی تاریخ معماری ایران</h6>
                   <button className="go-to-project">
                     {" "}
@@ -44,7 +41,9 @@ const Projects = () => {
               </div>
               <div className="col-4">
                 <div className="single-project  ">
-                <span className="project-icon"><TbBinaryTree/></span>
+                  <span className="project-icon">
+                    <TbBinaryTree />
+                  </span>
                   <h6>هستان‌شناسی باغ ایرانی</h6>
                   <button className="go-to-project">
                     {" "}
@@ -54,7 +53,9 @@ const Projects = () => {
               </div>
               <div className="col-4">
                 <div className="single-project border-0">
-                <span className="project-icon"><IoIosGitNetwork/></span>
+                  <span className="project-icon">
+                    <IoIosGitNetwork />
+                  </span>
                   <h6>هستان‌شناسی تاریخ معماری منظر</h6>
                   <button className="go-to-project">
                     {" "}
@@ -66,7 +67,9 @@ const Projects = () => {
             <div className="row row-container border-0">
               <div className="col-4 ">
                 <div className="single-project  ">
-                <span className="project-icon"><FaChalkboardTeacher/></span>
+                  <span className="project-icon">
+                    <FaChalkboardTeacher />
+                  </span>
                   <h6>پیرنیا</h6>
                   <button className="go-to-project">
                     {" "}
@@ -76,7 +79,9 @@ const Projects = () => {
               </div>
               <div className="col-4">
                 <div className="single-project  ">
-                <span className="project-icon"><BsReverseListColumnsReverse/></span>
+                  <span className="project-icon">
+                    <BsReverseListColumnsReverse />
+                  </span>
                   <h6>سینتوپیکون</h6>
                   <button className="go-to-project">
                     {" "}
@@ -86,7 +91,9 @@ const Projects = () => {
               </div>
               <div className="col-4">
                 <div className="single-project border-0">
-                <span className="project-icon"><SlNotebook/></span>
+                  <span className="project-icon">
+                    <SlNotebook />
+                  </span>
                   <h6>درس‌افزار</h6>
                   <button className="go-to-project">
                     <NavLink to="/darsafzaar-home">ورود</NavLink>
